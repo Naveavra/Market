2f4ce904c62cd0837f3222c57b6900bf1eded0f2
Itay <itayg8676@gmail.com>
2022-06-16 14:39:58 +0300

    removed redundant singletons

--- dev/src/DomainLayer/Employees/JobController.java
+++ dev/src/DomainLayer/Employees/JobController.java
@@ -4,20 +4,10 @@ import DAL.JobDAO;
 import java.util.*;
 
 public class JobController {
-//    private static final JobController instance = new JobController();
-//    private final Map<JobType, List<Employee>> roles;
+
     private final JobDAO jobDAO = new JobDAO();
 
     public JobController(){
-//        roles = new HashMap<>();
-//        roles.put(JobType.HR_MANAGER, new ArrayList<>());
-//        roles.put(JobType.SHIFT_MANAGER, new ArrayList<>());
-//        roles.put(JobType.CASHIER, new ArrayList<>());
-//        roles.put(JobType.STOCK_KEEPER, new ArrayList<>());
-//        roles.put(JobType.DRIVER, new ArrayList<>());
-//        roles.put(JobType.MERCHANDISER, new ArrayList<>());
-//        roles.put(JobType.LOGISTICS_MANAGER, new ArrayList<>());
-//        roles.put(JobType.TRANSPORT_MANAGER, new ArrayList<>());
     }
 
     public Set<Employee> getCertifiedEmployees(JobType role){
